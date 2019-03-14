0a9bfb9f629644a6d307b5f08c839ba463c39946
Gerardo Ovando <govando-2013109@kinal.edu.gt>
2019-03-14 16:52:49 -0600

    Agregar mas assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { GaleriaComponent } from './pages/galeria/galeria.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    GaleriaComponent
+    GaleriaComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
